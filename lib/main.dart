fea96fc779acf11dd3855bf471b7b8a79b4d058e
FaidraA <f.anthopoulou28@gmail.com>
2022-03-11 21:22:31 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'EMPer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+        primarySwatch: Colors.red,
+        appBarTheme:const AppBarTheme(
+         backgroundColor: Color(0xFF741818),
+         iconTheme: IconThemeData(color: Colors.white) ,
+         actionsIconTheme: IconThemeData(color: Colors.white),
+         centerTitle: false,
+         titleTextStyle: TextStyle(color: Colors.white, fontFamily: 'Pacifico', fontSize: 16.0),
+       )
+       
+  
+       ),
       home: const Splash(),
     );
   }
