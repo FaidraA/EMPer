743555835ccd4d6b0cc7399e72cb6f03cad56857
Dimitris Sarantidis <32545354+dimisarantidis@users.noreply.github.com>
2022-03-18 23:08:15 +0200

    Update log_in.dart

--- lib/screens/log_in.dart
+++ lib/screens/log_in.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LogIn extends StatefulWidget {
-  const LogIn({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const LogIn({Key? key}) : super(key: key);
   @override
   State<LogIn> createState() => _LogInState();
 }
@@ -19,16 +18,33 @@ class _LogInState extends State<LogIn> {
       home: Scaffold(
         backgroundColor: const Color(0xFF741818),
         body: Container(
-          //decoration: BoxDecoration(color: Colors.white),
+          alignment: Alignment.topCenter,
           child: Column(
             children: <Widget>[
-              Image.asset('assets/EMPerIcon.png'),
-              Text('EMPer'),
+              Image.asset(
+                'assets/EMPerIcon.png',
+                scale: 2,
+              ),
+              Text(
+                'EMPer',
+                style: GoogleFonts.pacifico(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
               SizedBox(
                 width: 200.0,
                 height: 100.0,
                 child: DecoratedBox(
                   decoration: BoxDecoration(color: Colors.white),
+                ),
+              ),
+              Text(
+                'Create an Account',
+                style: GoogleFonts.pacifico(
+                  fontSize: 16,
+                  color: Colors.white,
+                  decoration: TextDecoration.underline,
                 ),
               ),
             ],
