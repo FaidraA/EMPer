9e0aa59e3e9b98908e9a6a84642be6cd8d0c6535
Dimitris Sarantidis <32545354+dimisarantidis@users.noreply.github.com>
2022-03-20 21:04:24 +0200

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -4,3 +4,12 @@ int profileCount = 1;
 var alreadySeen = [false, false, false];
 var matchedProfile = [false, false, false];
 int viewingprofile = 4;
+
+String userMail = 'globals timi';
+String password = 'hi';
+String description = 'hi';
+
+String gender = 'hi';
+String datingPref = 'hi';
+String relationship = 'hi';
+String ntuaSchool = 'hi';
