6bf9f27cd0f8a1cd78b524fc8de1865796b7a8c0
FaidraA <f.anthopoulou28@gmail.com>
2022-03-20 16:49:19 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -630,95 +630,329 @@ class Chat extends StatefulWidget {
   ChatState createState() => ChatState();
 }
 
-class ChatState extends State<Chat> {
+class ChatState extends State<Chat>{
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp();
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Text Your Match',
+      home: Scaffold(
+        appBar: AppBar( 
+          title: Text('Chat!', style: GoogleFonts.getFont('Pacifico'),),
+          backgroundColor: const Color(0xFF741818),
+          leading: GestureDetector(
+            onTap: ()
+               {Navigator.pop(context);},
+            child: const Icon(Icons.close,)
+            ),
+             actions: [
+               IconButton(
+              onPressed: (){
+                 Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Call()),
+                );
+               }, 
+              icon: const Icon(
+                Icons.call, 
+                color: Colors.white
+                )
+              ),
+               IconButton(
+              onPressed: (){
+                 Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const NotificationsPage()),
+                );
+               }, 
+              icon: const Icon(
+                Icons.notifications, 
+                color: Colors.white
+                )
+              ),
+          ],
+          ), 
+          backgroundColor: const Color(0xFFEFDBD6),
+          body: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                if (globals.viewingprofile == 1) ...[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(backgroundImage: AssetImage('assets/Face1.png'),),
+                          Container(width: 10,),
+                          Container(
+                            decoration: BoxDecoration(
+                             borderRadius: BorderRadius.all(Radius.circular(20)),
+                             color: Color(0xFFAF302C),
+                            ),
+                            width: 200,
+                            height: 60,
+                            child: Text('\n Hi! How are you?', style: TextStyle(color: Colors.white))
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [                         
+                          Container(
+                            decoration: BoxDecoration(
+                             borderRadius: BorderRadius.all(Radius.circular(20)),
+                             color: Color(0xFFFC4C29),
+                            ),
+                            width: 200,
+                            height: 60,
+                            child: Text('\n Hi. I am fine, how about you?',style: TextStyle(color: Colors.white) ),
+                          ),
+                          Container(width: 10,),
+                          CircleAvatar(backgroundImage: AssetImage('assets/dummyprofile.jpg'),),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(backgroundImage: AssetImage('assets/Face1.png'),),
+                          Container(width: 10,),
+                          Container(
+                            decoration: BoxDecoration(
+                             borderRadius: BorderRadius.all(Radius.circular(20)),
+                             color: Color(0xFFAF302C),
+                            ),
+                            width: 260,
+                            height: 100,
+                            child: Text('\n I am fine too, even though a bit tired. I was working on an assignment for my Human Computer Interaction class until late last night', style: TextStyle(color: Colors.white)),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        height: 40,
+                        width: 300,
+                        child:TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(), 
+                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xFF741818))),
+                          hintText: 'Enter your message here',
+                          focusColor: Color(0xFF741818), 
+                          suffixIcon: Icon(Icons.add)), 
+                          cursorColor: Color(0xFF741818),                           
+                      ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(backgroundColor: const Color(0xFF741818)),
+                        onPressed: () {}, 
+                        child: Text('SEND...', style: GoogleFonts.pacifico(color: Colors.white)))
+                    ],
+                  )
+                ] else if (globals.viewingprofile == 2) ...[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(backgroundImage: AssetImage('assets/Face2.png'),),
+                          Container(width: 10,),
+                          Container(
+                            decoration: BoxDecoration(
+                             borderRadius: BorderRadius.all(Radius.circular(20)),
+                             color: Color(0xFFAF302C),
+                            ),
+                            width: 200,
+                            height: 60,
+                            child: Text('\n Hello. Whats up?', style: TextStyle(color: Colors.white))
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [                         
+                          Container(
+                            decoration: BoxDecoration(
+                             borderRadius: BorderRadius.all(Radius.circular(20)),
+                             color: Color(0xFFFC4C29),
+                            ),
+                            width: 200,
+                            height: 60,
+                            child: Text('\n Hiiii. I am just watching some Netflix, how about you?',style: TextStyle(color: Colors.white) ),
+                          ),
+                          Container(width: 10,),
+                          CircleAvatar(backgroundImage: AssetImage('assets/dummyprofile.jpg'),),
+                        ],
+                      ),                     
+                      Container(
+                        height: 40,
+                        width: 300,
+                        child:TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(), 
+                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xFF741818))),
+                          hintText: 'Enter your message here',
+                          focusColor: Color(0xFF741818), 
+                          suffixIcon: Icon(Icons.add)), 
+                          cursorColor: Color(0xFF741818),                           
+                      ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(backgroundColor: const Color(0xFF741818)),
+                        onPressed: () {}, 
+                        child: Text('SEND...', style: GoogleFonts.pacifico(color: Colors.white)))
+                    ],
+                  )
+                ] else if (globals.viewingprofile ==3) ...[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [                    
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [                         
+                          Container(
+                            decoration: BoxDecoration(
+                             borderRadius: BorderRadius.all(Radius.circular(20)),
+                             color: Color(0xFFFC4C29),
+                            ),
+                            width: 200,
+                            height: 60,
+                            child: Text('\n Hey! I think I saw you today on 242',style: TextStyle(color: Colors.white) ),
+                          ),
+                          Container(width: 10,),
+                          CircleAvatar(backgroundImage: AssetImage('assets/dummyprofile.jpg'),),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(backgroundImage: AssetImage('assets/Face3.png'),),
+                          Container(width: 10,),
+                          Container(
+                            decoration: BoxDecoration(
+                             borderRadius: BorderRadius.all(Radius.circular(20)),
+                             color: Color(0xFFFA302C),
+                            ),
+                            width: 200,
+                            height: 60,
+                            child: Text('\n I saw you too. I could not take my eyes off you', style: TextStyle(color: Colors.white)),
+                          ),
+                        ],
+                      ),                     
+                      Container(                       
+                        height: 40,
+                        width: 300,
+                        child:TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(), 
+                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xFF741818))),
+                          hintText: 'Enter your message here',
+                          focusColor: Color(0xFF741818), 
+                          suffixIcon: Icon(Icons.add)), 
+                          cursorColor: Color(0xFF741818),                           
+                      ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(backgroundColor: const Color(0xFF741818)),
+                        onPressed: () {}, 
+                        child: Text('SEND...', style: GoogleFonts.pacifico(color: Colors.white)))
+                    ],
+                  )
+                ],
+              ],
+            ),
+          ),
+      )
+    );
   }
 }
 
+class Call extends StatefulWidget {
+  const Call({Key? key}) : super(key: key);
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+  @override
+  CallState createState() => CallState();
+}
 
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
+class CallState extends State<Call>{
 
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+       title: 'Make a call',
+      home: Scaffold(
+        appBar: AppBar( 
+          title: Text('Calling...', style: GoogleFonts.getFont('Pacifico'),),
+          backgroundColor: const Color(0xFF741818),          
+          ),
+          backgroundColor: Color(0xFFFECABD),
+          body: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                if (globals.viewingprofile==1) ...[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(child: Image.asset('assets/Face1.png')),
+                      Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          onPressed: ( ) {},
+                          icon:Icon(Icons.mic, color: Color(0xFF741818),)),
+                        Container(width: 100,),
+                        IconButton(
+                          onPressed: (){Navigator.pop(context);}, 
+                          icon: Icon (Icons.cancel_sharp, color: Color(0xFFAF302C),))
+                      ],   
+                      )
+                    ],
+                  )
+                ] else if (globals.viewingprofile ==2) ...[
+                   Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(child: Image.asset('assets/Face2.png')),
+                      Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          onPressed: ( ) {},
+                          icon:Icon(Icons.mic, color: Color(0xFF741818),)),
+                        Container(width: 100,),
+                        IconButton(
+                          onPressed: (){Navigator.pop(context);}, 
+                          icon: Icon (Icons.cancel_sharp, color: Color(0xFFAF302C),))
+                      ],   
+                      )
+                    ],
+                  )
+                ] else if (globals.viewingprofile ==3) ...[
+                   Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(child: Image.asset('assets/Face3.png')),
+                      Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          onPressed: ( ) {},
+                          icon:Icon(Icons.mic, color: Color(0xFF741818),)),
+                        Container(width: 100,),
+                        IconButton(
+                          onPressed: (){Navigator.pop(context);}, 
+                          icon: Icon (Icons.cancel_sharp, color: Color(0xFFAF302C),))
+                      ],   
+                      )
+                    ],
+                  )
+                ]
+              ],
+            ),
+          ),
+      ),
+    );
+  }
+}
 
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-//}
